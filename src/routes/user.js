f288e452c29f436524be64c09ac3710181b9c21b
datleDev04 <letiendat040904@gmail.com>
2024-06-30 23:12:01 +0700

    fix/validate roles of user when and populate roles

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { UserController } from "../controllers/user.controller.js";
+import { userValidation } from "../validations/userValidation.js";
 
 const userRouter = express.Router();
 
 
 
 // only admin role can access
-userRouter.patch('/:id' , UserController.updateUser)
+userRouter.patch('/:id' , userValidation.updateUserInfo ,UserController.updateUser)
 
 export default userRouter
---
+++ src/validations/userValidation.js
@@ -0,0 +1,27 @@
+import { StatusCodes } from "http-status-codes";
+import ApiError from "../utils/ApiError.js";
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE, validateBeforeCreateOrUpdate } from "../utils/validators.js";
+import Joi from "joi";
+
+export class userValidation  {
+
+  static updateUserInfo = async (req, res, next) => {
+     const correctCondition = Joi.object({
+         roles: Joi.alternatives().try(
+           Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
+           Joi.array().items(Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE))
+         )
+       });
+   
+       try {
+         await validateBeforeCreateOrUpdate(correctCondition, req.body);
+         next();
+       } catch (error) {
+         next(
+           new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, new Error(error).message)
+         );
+       }
+  }
+
+}
+
