d28ec8f09bf7ce41850f9e0be87eb3861fbe872f
datleDev04 <letiendat040904@gmail.com>
2024-07-01 11:05:40 +0700

    refactor role, update user controller success response

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,17 +1,20 @@
 import { StatusCodes } from "http-status-codes"
 import UserService from "../services/user.service.js"
 import { Transformer } from "../utils/transformer.js"
+import { SuccessResponse } from "../utils/response.js"
 
 export class UserController {
     static updateUser = async (req, res, next) => {
         try {
             const updatedUser = await UserService.updateUser(req)
 
-            res.status(StatusCodes.OK).json({
-                message: "Updated User successfully",
-                statusCode: StatusCodes.OK,
-                metaData: Transformer.transformObjectTypeSnakeToCamel(updatedUser.toObject())
-            })
+
+            SuccessResponse(
+                res,
+                StatusCodes.OK,
+                "Updated User successfully",
+                Transformer.transformObjectTypeSnakeToCamel(updatedUser.toObject())
+            ) 
         } catch (error) {
             next(error)
         }
