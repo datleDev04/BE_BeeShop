9d3970ff60058d09e1aee23810155c5ddf77812e
PhongLB_2004 <luongphong.dev@gmail.com>
2024-07-02 07:02:04 +0700

    feat: CRUD brand module

---
+++ src/services/brand.service.js
@@ -0,0 +1,58 @@
+import { StatusCodes } from 'http-status-codes';
+import Brand from '../models/Brand.js';
+import ApiError from '../utils/ApiError.js';
+
+export default class BrandService {
+  static handleGetAllBrand = async (req) => {
+    const brands = await Brand.find();
+    return brands;
+  };
+
+  static handleGetOneBrand = async (req) => {
+    const brand = await Brand.findById(req.params.id);
+    return brand;
+  };
+
+  static handleCreateBrand = async (req) => {
+    const { name, image, description } = req.body;
+
+    const existedBrand = await Brand.findOne({ name });
+
+    if (existedBrand) {
+      throw new ApiError(StatusCodes.CONFLICT, 'This brand is existed');
+    }
+
+    const newBrand = await Brand.create({
+      name,
+      image,
+      description,
+    });
+
+    return newBrand;
+  };
+
+  static handleUpdateBrand = async (req) => {
+    const { name, image, description } = req.body;
+
+    const updateBrand = await Brand.findByIdAndUpdate(req.params.id, { name, image, description }, { new: true, runValidators: true });
+
+    if (!updateBrand) {
+      throw new ApiError(StatusCodes.CONFLICT, 'This brand is not available');
+    }
+
+    return updateBrand;
+  };
+
+  static handleDeleteBrand = async (req) => {
+    const { id } = req.params;
+
+    const brand = await Brand.findById(id);
+
+    if (!brand) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'Brand not found');
+    }
+    await Brand.findByIdAndDelete(id);
+
+    return brand;
+  };
+}
