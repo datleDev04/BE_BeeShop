3b7b0b6f1179f8ae252685c531ed88a3d705dc13
datleDev04 <letiendat040904@gmail.com>
2024-07-02 21:04:15 +0700

    feat: crud tags

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import roleRouter from './role.js';
 import permissionRouter from './permission.js';
 import userRouter from './user.js';
 import brandRouter from './brand.js';
+import tagRouter from './tag.js';
 
 const router = express.Router();
 
@@ -12,5 +13,6 @@ router.use('/users', userRouter);
 router.use('/roles', roleRouter);
 router.use('/permissions', permissionRouter);
 router.use('/brands', brandRouter);
+router.use('/tags', tagRouter);
 
 export default router;
