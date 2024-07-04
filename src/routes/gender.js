0f362cba79b7e88fabc4a3fe82ff4a3695b31e25
datleDev04 <letiendat040904@gmail.com>
2024-07-04 10:44:29 +0700

    feat: return user, accessToken, permissions into req.user

--- src/routes/gender.js
+++ src/routes/gender.js
@@ -2,12 +2,13 @@ import express from 'express';
 import { GenderController } from '../controllers/gender.controller.js';
 import { genderValidation } from '../validations/genderValidation.js';
 import { objectIdValidation } from '../validations/objectIdValidation.js';
+import { authMiddleware } from '../middleware/authMiddleware.js';
 
 const genderRouter = express.Router();
 
 genderRouter.get('/', GenderController.getAllGenders);
-genderRouter.get('/:id', objectIdValidation, GenderController.getGender);
-genderRouter.post('/', genderValidation, GenderController.createNewGender);
+genderRouter.get('/:id',objectIdValidation, GenderController.getGender);
+genderRouter.post('/', authMiddleware,genderValidation, GenderController.createNewGender);
 genderRouter.patch('/:id', objectIdValidation, genderValidation, GenderController.updateGender);
 genderRouter.delete('/:id', objectIdValidation, GenderController.deleteGender);
 
