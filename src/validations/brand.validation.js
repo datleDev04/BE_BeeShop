b98380456d35c360bf95ccd6606033b19a8f9dcf
datleDev04 <letiendat040904@gmail.com>
2024-07-21 00:44:02 +0700

    fix: allow empty description of brand

--- src/validations/brand.validation.js
+++ src/validations/brand.validation.js
@@ -1,41 +1,12 @@
-import { StatusCodes } from 'http-status-codes';
-import ApiError from '../utils/ApiError.js';
 import { validateBeforeCreateOrUpdate } from '../utils/validators.js';
 import Joi from 'joi';
 
-export const createBrandValidation = async (req, res, next) => {
-  const correctCondition = Joi.object({
-    name: Joi.string().trim().required().messages({
-      'string.empty': "Brand name can't be empty",
-      'any.required': 'Brand name is required',
-    }),
-    image: Joi.string().trim().messages({
-      'string.empty': "Brand image can't be empty",
-    }),
-    description: Joi.string().trim().messages({
-      'string.empty': "Brand description can't be empty",
-    }),
-  });
 
-  try {
-    await validateBeforeCreateOrUpdate(correctCondition, req.body);
-    next();
-  } catch (error) {
-    next(error);
-  }
-};
-
-export const updateBrandValidation = async (req, res, next) => {
+export const brandValidation = async (req, res, next) => {
   const correctCondition = Joi.object({
-    name: Joi.string().trim().messages({
-      'string.empty': "Brand name can't be empty",
-    }),
-    image: Joi.string().trim().messages({
-      'string.empty': "Brand image can't be empty",
-    }),
-    description: Joi.string().trim().messages({
-      'string.empty': "Brand description can't be empty",
-    }),
+    name: Joi.string().trim().required(),
+    image: Joi.string().trim().required(),
+    description: Joi.string().allow("").trim()
   });
 
   try {
