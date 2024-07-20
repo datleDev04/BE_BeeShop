53801f255d9bc7038e86041165e897c1804e6ce5
Xuan Tho <tholxph31196@fpt.edu.vn>
2024-07-20 12:00:33 +0700

    feature: paginate, search, check exist order-status

--- src/services/order_status.service.js
+++ src/services/order_status.service.js
@@ -1,47 +1,60 @@
 import { StatusCodes } from 'http-status-codes';
 import ApiError from '../utils/ApiError.js';
 import OrderStatus from '../models/Order_Status.js';
+import { getFilterOptions, getPaginationOptions } from '../utils/pagination.js';
+import { Transformer } from '../utils/transformer.js';
+import { checkRecordByField } from '../utils/CheckRecord.js';
 
 export class OrderStatusService {
   static getAllOrderStatus = async (req) => {
-    return await OrderStatus.find().sort({ createdAt: -1 });
+    const options = getPaginationOptions(req);
+    const filter = getFilterOptions(req, ['name']);
+
+    const paginatedLabels = await OrderStatus.paginate(filter, options);
+
+    const { docs, ...otherFields } = paginatedLabels;
+
+    const transformedLabels = docs.map((label) =>
+      Transformer.transformObjectTypeSnakeToCamel(label.toObject())
+    );
+
+    const others = {
+      ...otherFields,
+    };
+
+    return {
+      metaData: Transformer.removeDeletedField(transformedLabels),
+      others,
+    };
   };
 
   static getOneOrderStatus = async (req) => {
-    const orderStatus = await OrderStatus.findById(req.params.id);
+    await checkRecordByField(OrderStatus, '_id', req.params.id, true);
+    const orderStatus = OrderStatus.findById(req.params.id);
 
-    if (!orderStatus) {
-      throw new ApiError(StatusCodes.NOT_FOUND, 'Order Status not found');
-    }
-
-    return orderStatus;
+    return Transformer.transformObjectTypeSnakeToCamel(orderStatus);
   };
 
   static createOrderStatus = async (req) => {
     const { name, description } = req.body;
 
-    const existingOrderStatus = await OrderStatus.findOne({ name });
-
-    if (existingOrderStatus) {
-      throw new ApiError(StatusCodes.CONFLICT, 'Order Status already exists');
-    }
+    await checkRecordByField(OrderStatus, 'name', name, false);
 
     const newOrderStatus = await OrderStatus.create({
       name,
       description,
     });
 
-    return newOrderStatus;
+    return Transformer.transformObjectTypeSnakeToCamel(newOrderStatus.toObject());
   };
 
   static updateOrderStatusById = async (req) => {
     const { name, description } = req.body;
 
-    const existingOrderStatus = await OrderStatus.findOne({ name });
+    const { id } = req.params;
+    await checkRecordByField(OrderStatus, 'name', name, false, id);
 
-    if (existingOrderStatus) {
-      throw new ApiError(StatusCodes.CONFLICT, 'Order Status already exists');
-    }
+    await checkRecordByField(OrderStatus, '_id', id, true);
 
     const updatedOrderStatus = await OrderStatus.findByIdAndUpdate(
       req.params.id,
@@ -59,16 +72,12 @@ export class OrderStatusService {
       throw new ApiError(StatusCodes.CONFLICT, 'This payment status is not existing');
     }
 
-    return updatedOrderStatus;
+    return Transformer.transformObjectTypeSnakeToCamel(updatedOrderStatus.toObject());
   };
 
   static deleteOrderStatusBydId = async (req) => {
-    const deletedOrderStatus = await OrderStatus.findByIdAndDelete(req.params.id);
-
-    if (!deletedOrderStatus) {
-      throw new ApiError(404, 'Order status not found');
-    }
-
-    return deletedOrderStatus;
+    const { id } = req.params;
+    await checkRecordByField(OrderStatus, '_id', id, true);
+    await OrderStatus.findByIdAndDelete(id);
   };
 }
