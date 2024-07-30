e9654e2b70a32d0ed7e65f86b7f29563dae183c5
vnedd <thangvu2098@gmail.com>
2024-07-30 21:55:00 +0700

    refactor: add slug to gender module

--- src/validations/gender.validation.js
+++ src/validations/gender.validation.js
@@ -1,10 +1,8 @@
-import { StatusCodes } from 'http-status-codes';
-import ApiError from '../utils/ApiError.js';
 import { validateBeforeCreateOrUpdate } from '../utils/validators.js';
 import Joi from 'joi';
 
-export const genderValidation = async (req, res, next) => {
-  const correctCondition = Joi.object({
+export const createGenderValidation = async (req, res, next) => {
+  const createSchema = Joi.object({
     name: Joi.string().trim().required().messages({
       'string.empty': "Gender name can't be empty",
       'any.required': 'Gender name is required',
@@ -12,7 +10,22 @@ export const genderValidation = async (req, res, next) => {
   });
 
   try {
-    await validateBeforeCreateOrUpdate(correctCondition, req.body);
+    await validateBeforeCreateOrUpdate(createSchema, req.body);
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateGenderValidation = async (req, res, next) => {
+  const updateSchema = Joi.object({
+    name: Joi.string().trim().messages({
+      'string.empty': "Gender name can't be empty",
+    }),
+  });
+
+  try {
+    await validateBeforeCreateOrUpdate(updateSchema, req.body);
     next();
   } catch (error) {
     next(error);
