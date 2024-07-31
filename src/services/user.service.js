089f9c870808971e8f372c45cceea180e74aaa0b
Xuan Tho <tholx2811workspace@gmail.com>
2024-07-31 23:28:16 +0700

    fix[update-user]

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -87,7 +87,7 @@ export default class UserService {
       addresses,
       tags,
     } = req.body;
-    await checkRecordByField(User, 'user_name', user_name, false, req.user._id);
+    await checkRecordByField(User, 'user_name', user_name, false, req.params.id);
     await checkRecordByField(User, 'email', email, false, req.user._id);
 
     const userPermissions = req.user.roles.flatMap((role) =>
