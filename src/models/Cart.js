91716aeef2fc00ad08b8803e038b33438d4401e3
Xuan Tho <tholx2811workspace@gmail.com>
2024-08-12 21:25:32 +0700

    fix/tho/response cart

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -6,7 +6,7 @@ const COLLECTION_NAME = 'Carts';
 
 const cartSchema = mongoose.Schema(
   {
-    user_id: {
+    user: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
       ref: 'User',
