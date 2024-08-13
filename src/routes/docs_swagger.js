2c323ebb7c353fb8ab8fa40e7bc949df447cb2d2
PhongLB_2004 <luongphong.dev@gmail.com>
2024-08-13 10:53:48 +0700

    fix: del cmt

--- src/routes/docs_swagger.js
+++ src/routes/docs_swagger.js
@@ -6,7 +6,6 @@ import path from 'path';
 
 import { fileURLToPath } from 'url';
 
-// Get the directory name of the current module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
