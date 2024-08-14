ec07157f96015aa4f2f0e7b2296c83911aefa4b7
Xuan Tho <tholx2811workspace@gmail.com>
2024-08-14 23:20:53 +0700

    fix/tho/create-user

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -112,12 +112,10 @@ export class userValidation {
           Joi.array().items(Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE))
         )
         .optional(),
-      addresses: Joi.alternatives()
-        .try(
-          Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
-          Joi.array().items(Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE))
-        )
-        .optional(),
+      commune: Joi.string().trim().required(),
+      district: Joi.string().trim().required(),
+      city: Joi.string().trim().required(),
+      detail_address: Joi.string().trim().required(),
       vouchers: Joi.alternatives()
         .try(
           Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
