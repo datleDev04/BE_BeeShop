4f46f60cf95bc759213543c18b5fd6934ae07f90
datleDev04 <letiendat040904@gmail.com>
2024-10-09 04:43:46 +0700

    color: get all and get one color

--- src/routes/client/index.js
+++ src/routes/client/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import bannerClientRouter from "./banner.client.js";
 import productClientRouter from "./product.client.js";
+import colorClientRouter from "./color.client.js";
 
 const clientRouter = express.Router();
 
 clientRouter.use('/banner', bannerClientRouter);
 clientRouter.use('/product', productClientRouter);
+clientRouter.use('/color', colorClientRouter);
 
 export default clientRouter
