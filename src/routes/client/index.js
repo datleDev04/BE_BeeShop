15e2845fbaa8f2ae9ec81366173a17ca1c618a5b
datleDev04 <letiendat040904@gmail.com>
2024-10-09 04:50:32 +0700

    product-type: get all and get one product type

--- src/routes/client/index.js
+++ src/routes/client/index.js
@@ -3,6 +3,7 @@ import bannerClientRouter from "./banner.client.js";
 import productClientRouter from "./product.client.js";
 import colorClientRouter from "./color.client.js";
 import sizeClientRouter from "./size.client.js";
+import productTypeClientRouter from "./product_type.client.js";
 
 const clientRouter = express.Router();
 
@@ -10,5 +11,6 @@ clientRouter.use('/banner', bannerClientRouter);
 clientRouter.use('/product', productClientRouter);
 clientRouter.use('/color', colorClientRouter);
 clientRouter.use('/size', sizeClientRouter);
+clientRouter.use('/product-type', productTypeClientRouter);
 
 export default clientRouter
