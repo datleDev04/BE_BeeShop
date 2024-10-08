949f358f189fc23d99e42f1e91b8910b928223ba
datleDev04 <letiendat040904@gmail.com>
2024-10-09 04:46:48 +0700

    size: get all and get one size

--- src/routes/client/index.js
+++ src/routes/client/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import bannerClientRouter from "./banner.client.js";
 import productClientRouter from "./product.client.js";
 import colorClientRouter from "./color.client.js";
+import sizeClientRouter from "./size.client.js";
 
 const clientRouter = express.Router();
 
 clientRouter.use('/banner', bannerClientRouter);
 clientRouter.use('/product', productClientRouter);
 clientRouter.use('/color', colorClientRouter);
+clientRouter.use('/size', sizeClientRouter);
 
 export default clientRouter
