7911bb776073b4a1d341b5fc7295e36bb588c504
datleDev04 <letiendat040904@gmail.com>
2024-10-21 22:09:36 +0700

    feat: client get all and get one tag

--- src/routes/client/index.js
+++ src/routes/client/index.js
@@ -5,6 +5,7 @@ import colorClientRouter from "./color.client.js";
 import sizeClientRouter from "./size.client.js";
 import productTypeClientRouter from "./product_type.client.js";
 import wishListClientRouter from "./wishlist.client.js";
+import tagClientRouter from "./tag.client.js";
 
 const clientRouter = express.Router();
 
@@ -14,5 +15,6 @@ clientRouter.use('/color', colorClientRouter);
 clientRouter.use('/size', sizeClientRouter);
 clientRouter.use('/product-type', productTypeClientRouter);
 clientRouter.use('/wishlist', wishListClientRouter);
+clientRouter.use('/tag', tagClientRouter);
 
 export default clientRouter
