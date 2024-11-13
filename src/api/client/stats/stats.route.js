8130dd482641368df732921a315c1be7ec4c314f
baoanh <baoanh27042004@gmail.com>
2024-11-13 22:13:42 +0700

    .

--- src/api/client/stats/stats.route.js
+++ src/api/client/stats/stats.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { statsController } from './stats.controller.js';
 import { authMiddleware } from '../../../middleware/authMiddleware.js';
+import { CheckPermission } from '../../../utils/CheckPermission.js'
 const statsRouter = express.Router();
 
 statsRouter.get('/most-purchased-size', authMiddleware, CheckPermission(['Read_Stats']), statsController.getMostPurchasedSize);
