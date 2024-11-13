1576a2b3c3a6a4a85e21eed5c6de6aaa1bab904b
baoanh <baoanh27042004@gmail.com>
2024-11-13 22:12:35 +0700

    update permission

--- src/api/client/stats/stats.route.js
+++ src/api/client/stats/stats.route.js
@@ -3,7 +3,7 @@ import { statsController } from './stats.controller.js';
 import { authMiddleware } from '../../../middleware/authMiddleware.js';
 const statsRouter = express.Router();
 
-statsRouter.get('/most-purchased-size', authMiddleware, statsController.getMostPurchasedSize);
-statsRouter.get('/most-purchased-color', authMiddleware, statsController.getMostPurchasedColor);
+statsRouter.get('/most-purchased-size', authMiddleware, CheckPermission(['Read_Stats']), statsController.getMostPurchasedSize);
+statsRouter.get('/most-purchased-color', authMiddleware, CheckPermission(['Read_Stats']), statsController.getMostPurchasedColor);
 
 export { statsRouter };
