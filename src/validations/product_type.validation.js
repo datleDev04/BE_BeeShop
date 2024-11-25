984d414b7a249a4c98c2b3cafc51bb35fb9944dc
vnedd <thangvu2098@gmail.com>
2024-11-25 23:20:48 +0700

    add image to product type

--- src/validations/product_type.validation.js
+++ src/validations/product_type.validation.js
@@ -7,6 +7,10 @@ export const createProductTypeValidation = async (req, res, next) => {
       'string.empty': "Product type name can't be empty",
       'any.required': 'Product type name is required',
     }),
+    image_url: Joi.string().trim().required().messages({
+      'string.empty': "Image Url can't be empty",
+      'any.required': 'Image Url is required',
+    }),
   });
 
   try {
@@ -22,7 +26,11 @@ export const updateProductTypeValidation = async (req, res, next) => {
     name: Joi.string().trim().messages({
       'string.empty': "Product type name can't be empty",
     }),
-  }).min(1);
+    image_url: Joi.string().trim().messages({
+      'string.empty': "Image Url can't be empty",
+      'any.required': 'Image Url is required',
+    }),
+  });
   try {
     await validateBeforeCreateOrUpdate(updateSchema, req.body);
     next();
